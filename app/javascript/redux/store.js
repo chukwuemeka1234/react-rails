0bbc0420c3451f2f52ea94a3018e9542ce5cb68f
chukwuemeka1234 <anivictor.chukwuemeka.avc@gmail.com>
2022-05-14 15:19:13 +0100

    Add react, redux webpack

---
+++ app/javascript/redux/store.js
@@ -0,0 +1,14 @@
+import { createStore, applyMiddleware, combineReducers } from 'redux';
+import thunk from 'redux-thunk';
+import greetingsReducer from "./greetings/greetings"
+
+const rootReducer = combineReducers({
+    greeting: greetingsReducer,
+});
+
+const store = createStore(
+    rootReducer,
+    applyMiddleware(thunk)
+);
+
+export default store;
